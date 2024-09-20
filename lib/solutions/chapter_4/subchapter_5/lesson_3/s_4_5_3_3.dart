ee1c6387e4b680d9eb500b4af78fc64160588ccf
arthur Windows <rybalkin.arthur@gmail.com>
2024-09-20 11:58:49 +0200

    ts 4.3.2 wichtige widgets II

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
@@ -4,6 +4,14 @@ class S4533 extends StatelessWidget {
   const S4533({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Container(
+      color: Colors.blue,
+      height: 150,
+      width: 150,
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text("App"), Text("Akademie")],
+      ),
+    );
   }
 }
--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
@@ -4,6 +4,29 @@ class S4535 extends StatelessWidget {
   const S4535({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const Column(
+      children: [
+        Row(
+          children: [ColoredContainer(color: Colors.red), ColoredContainer(color: Colors.green)],
+        ),
+        Row(
+          children: [ColoredContainer(color: Colors.blue), ColoredContainer(color: Colors.yellow)],
+        )
+      ],
+    );
+  }
+}
+
+class ColoredContainer extends StatelessWidget {
+  final Color color;
+  const ColoredContainer({super.key, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+      color: color,
+    );
   }
 }
